e4117097d083a2192033f2a67044b341cb970402
Mauricio Lopez Alvarez <mauriciolopezalvarez@Mauricios-MBP.attlocal.net>
2024-05-15 11:02:12 -0400

    First commit

---
+++ LMS.java
@@ -0,0 +1,21 @@
+/*
+ * Author: Mauricio Lopez Alvarez
+ * Date Written: May 15, 2023
+ * Purpose:... I have been asked to create a simple Library Management System(LMS). Features that will be
+ *             implemented in the software are adding a book, removing a book in the list, and displaying
+ *             all current books in the system.
+ */
+
+import java.util.*;
+
+public class LMS
+{
+    public static void main(String[] args)
+    {
+        // Variables and Constants
+
+        // Welcoming Message
+        System.out.println("Welcome to Library Management System.");
+
+    } // end Main
+} // end LMS class
